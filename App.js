052b4ce9ea6e8f5203bcefb7df664bef382290c2
Marcus Vinicius <marcusvslourenco@fac.pe.senac.br>
2022-11-18 18:48:50 -0300

    Imagem adicionada e texto atualizado

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image style={styles.image} source={require('./assets/favicon.png')}/>
+      <Text>Marcus Vinicius Santana Lourenço</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: 'stretch',
   },
 });
